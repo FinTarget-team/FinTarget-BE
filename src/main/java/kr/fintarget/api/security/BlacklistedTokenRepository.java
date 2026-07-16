520588ed494e9d95b33fd14abca4979ddfa94566
Min Jiwon <minwldnjs87@gmail.com>
2026-07-16 16:31:07 +0900

    feat: BlacklistedTokenRepository에 만료 토큰 조회 메서드 추가 (#39)

--- src/main/java/kr/fintarget/api/security/BlacklistedTokenRepository.java
+++ src/main/java/kr/fintarget/api/security/BlacklistedTokenRepository.java
@@ -2,5 +2,9 @@ package kr.fintarget.api.security;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface BlacklistedTokenRepository extends JpaRepository<BlacklistedToken, String> {
+    List<BlacklistedToken> findByExpiresAtBefore(LocalDateTime dateTime);
 }
